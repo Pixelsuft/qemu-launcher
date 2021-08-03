38c4636ee0e4c3770ca29f34c98fe77d44fe8e2d
pixelsuft <yuskivl07@mail.ru>
2021-08-03 12:53:04 +0700

    updates

--- setup_vm.py
+++ setup_vm.py
@@ -3,8 +3,6 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ui.sets import Ui_MainWindow
 import config
-import subprocess
-from threading import Thread
 import builtins
 
 
@@ -13,13 +11,23 @@ vm_path = vm + '.vm'
 conf = {}
 
 
+def s(param, value):
+    conf[param] = value
+
+
 def setup_events():
     ui.cancelButton.clicked.connect(lambda: sys.exit(0))
     ui.okButton.clicked.connect(lambda: sys.exit(on_exit(0)))
 
 
 def process_config(c):
-    print(c)
+    ui.archEdit.setCurrentIndex(int(c['arch'] == '64-bit'))
+    ui.cpu_edit.setCurrentText(conf['cpu'])
+
+
+def apply_config():
+    s('arch', ui.archEdit.currentText())
+    s('cpu', ui.cpu_edit.currentText())
 
 
 def on_init():
@@ -32,7 +40,8 @@ def on_init():
 
 
 def on_exit(to_exit_code):
-    config.write_config(vm_path, {})
+    apply_config()
+    config.write_config(vm_path, conf)
     if not to_exit_code == 0:
         print('Error exit!', to_exit_code)
     return to_exit_code
