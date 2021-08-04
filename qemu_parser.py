3eb3c024dc7a31004c33dc8c5b4510905894bcd1
pixelsuft <yuskivl07@mail.ru>
2021-08-04 10:47:33 +0700

    updates

--- qemu_parser.py
+++ qemu_parser.py
@@ -5,7 +5,7 @@ import signal
 import sys
 
 
-sets = builtins.qemu_conf['sets']
+s = builtins.qemu_conf['sets']
 pid = builtins.qemu_conf['pid']
 vm_name = builtins.qemu_conf['vm_name']
 boot_from = builtins.qemu_conf['boot_from']
@@ -15,9 +15,75 @@ args = []
 cmd = ''
 
 
+if s['sudo']:
+    args.append('sudo')
+args.append('qemu-system-' + ('i386' if s['arch'] == '32-bit' else 'x86_64')
+            + ('w' if (not s['useconsole'] and sys.platform == 'win32') else ''))
+if s['nodefaults']:
+    args.append('-nodefaults')
+args.append('-cpu')
+args.append(s['cpu'])
+args.append('-m')
+args.append(s["memory"])
+
+if not s['bios'] == 'BIOS':
+    bios = None
+    if s['bios'] == 'EFI 32-bit (CIRRUS ONLY)':
+        bios = 'efi32.bin'
+    elif s['bios'] == 'EFI 64-bit (CIRRUS ONLY)':
+        bios = 'efi64.bin'
+    elif s['bios'] == 'EFI 32-bit (FIXED)':
+        bios = 'efi32_fixed.bin'
+    args.append('-bios')
+    args.append(bios)
+
+if not s['cores'] == '-1':
+    args.append('-smp')
+    args.append(s['cores'])
+
+if not s['hpet']:
+    args.append('-no-hpet')
+
+if not s['acpi']:
+    args.append('-no-acpi')
+
+if s['kvm']:
+    args.append('-accel')
+    args.append('kvm,thread=single')
+if s['mtkvm']:
+    args.append('-accel')
+    args.append('kvm,thread=multi')
+if s['tcg']:
+    args.append('-accel')
+    args.append('tcg,thread=single')
+if s['mttcg']:
+    args.append('-accel')
+    args.append('tcg,thread=multi')
+if s['whpx']:
+    args.append('-accel')
+    args.append('whpx')
+if s['haxm']:
+    args.append('-accel')
+    args.append('hax')
+
+if not s['speed'] == '-1':
+    args.append('-rtc')
+    args.append('base=localtime,clock=vm')
+    args.append('-icount')
+    args.append(f'shift={s["speed"]},align=off,sleep=off')
+
+args.append('-no-reboot')
+args.append('-name')
+args.append(vm_name)
+print(args)
+
+
 def win_thread():
-    if not sys.platform == 'win32' or not sets['mytools']:
+    if not sys.platform == 'win32' or not s['mytools'] or (not s['sdl'] and not s['display'] == 'SDL'):
         return
+    import win32api
+    import win32con
+    import win32gui
     print('continue')
 
 
